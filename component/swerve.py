41eb16c8a8001e75bf6b23e87644d0e53d8c6c47
dublUayaychtee <mail@dublu.net>
2025-01-18 19:46:20 -0500

    add files

---
+++ component/swerve.py
@@ -0,0 +1,67 @@
+import wpilib
+import phoenix6
+
+from magicbot import feedback, will_reset_to
+
+from constant import TunerConstants, DriveConstants
+
+
+class Swerve(phoenix6.swerve.SwerveDrivetrain):
+
+    request = will_reset_to(
+        phoenix6.swerve.requests.Idle()
+    )  # default request if no request is given
+
+    def __init__(self):
+        phoenix6.swerve.SwerveDrivetrain.__init__(
+            self,
+            TunerConstants._drive_motor_class,
+            TunerConstants._steer_motor_class,
+            TunerConstants._encoder_class,
+            TunerConstants.drivetrain_constants,
+            [
+                TunerConstants.front_left,
+                TunerConstants.front_right,
+                TunerConstants.back_left,
+                TunerConstants.back_right,
+            ],
+        )
+
+        if phoenix6.utils.is_simulation():
+            ...
+
+    def go(self, x, y, z):  # convenience
+        self.request = (
+            phoenix6.swerve.requests.FieldCentric()
+            .with_deadband(DriveConstants.vel_deadband)
+            .with_rotational_deadband(DriveConstants.rot_deadband)
+            .with_drive_request_type(  # idk this was in the template code
+                phoenix6.swerve.SwerveModule.DriveRequestType.OPEN_LOOP_VOLTAGE
+            )
+            .with_velocity_x(x * DriveConstants.max_vel)
+            .with_velocity_y(y * DriveConstants.max_vel)
+            .with_rotational_rate(z * DriveConstants.max_rot)
+        )
+
+    def brake(self):
+        self.request = phoenix6.swerve.requests.SwerveDriveBrake()
+
+    def point(self):
+        self.request = phoenix6.swerve.requests.PointWheelsAt()
+
+    def set(self, request: phoenix6.swerve.requests.SwerveRequest):
+        self.request = request
+
+    # these values are pushed to NetworkTables
+    @feedback
+    def speed(self) -> list[float]:
+        return list(map(lambda x: x.speed, self.get_state().module_targets))
+
+    @feedback
+    def angle(self) -> list[float]:
+        return list(map(lambda x: x.angle.degrees(), self.get_state().module_targets))
+
+    # ran automatically (periodic)
+    def execute(self):
+        self.set_control(self.request)
+        print(self.get_state().module_targets[0].speed)
---
+++ robot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import wpilib
+import phoenix6
+from magicbot import MagicRobot
+
+from constant import TunerConstants, DriveConstants
+
+from component.swerve import Swerve
+
+
+class Robot(MagicRobot):
+
+    swerve: Swerve
+
+    def createObjects(self):
+        self.controller = wpilib.GenericHID(0)
+
+    def teleopPeriodic(self):
+        self.swerve.go(
+            self.controller.getRawAxis(0),
+            self.controller.getRawAxis(1),
+            self.controller.getRawAxis(2),
+        )
+
+        if self.controller.getRawButton(1):
+            self.swerve.brake()
