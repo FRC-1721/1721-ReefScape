7b1188aa9d11e69d2dba538e11a28577b51f9df9
dublUayaychtee <mail@dublu.net>
2025-01-20 19:17:16 -0500

    increase dampening because

--- constant/DriveConstants.py
+++ constant/DriveConstants.py
@@ -1,9 +1,9 @@
 from constant import TunerConstants
 
-vel_dampen = 0.3
+vel_dampen = 0.1
 max_vel = TunerConstants.speed_at_12_volts * vel_dampen
 vel_deadband = max_vel * 0.02
 
-rot_dampen = 1
+rot_dampen = 0.5
 max_rot = 2 * rot_dampen  # arbitrary value, idk
 rot_deadband = max_rot * 0.02
