48afe844b8baa5717dad303ef752e8d595991454
kredcool <kredcool2007@gmail.com>
2025-01-22 17:55:31 -0500

    fill out this code later

--- component/elevator.py
+++ component/elevator.py
@@ -5,33 +5,17 @@ from magicbot import feedback, will_reset_to
 class Elevator:
     elevatorMotor1: phoenix6.hardware.TalonFX
     elevatorMotor2: phoenix6.hardware.TalonFX
+    # encoder: ?
 
     x = will_reset_to(0)
 
     # TODO: Adjust PID
-    controller = wpimath.controller.HolonomicDriveController(
-        wpimath.controller.PIDController(1, 0, 0),
-        wpimath.controller.PIDController(1, 0, 0),
-        wpimath.controller.ProfiledPIDControllerRadians(
-            1, 0, 0, wpimath.trajectory.TrapezoidProfileRadians.Constraints(6.28, 3.14)
-        ),
-    )
+    controller = wpimath.controller.PIDController(1, 0, 0)
 
-    # def move(self, amount):
-    #     self.x = amount
-
-    # def execute(self):
-    #     motor.set(self.x)
-
-    def target(
-        self,
-        goal: wpimath.geometry.Pose2d,
-        velocity=0.5,
-        facing=wpimath.geometry.Rotation2d(0),
-    ):  # the formatter made this really tall
-        self.go(
-            *self.controller.calculate(self.get_state().pose, goal, velocity, facing)
-        )
+    def set(self, goal):
+        self.x = goal
 
     def execute(self):
-        pass
+        # elevatorMotor1.set(self.controller.calculate(
+        # encoder.pose, self.x
+        # ))
